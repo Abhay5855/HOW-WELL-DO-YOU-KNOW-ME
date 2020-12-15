d866fa0626576fc87ac29dfcf7bd56420354571d
Abhay Patil <ap3919529@gmail.com>
2020-12-15 03:04:59 +0000

    use: if condition to check if its equal to answer and update score

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ console.log("Welcome " + userName + " lets play a game!")
 score = 0;
 function Play(question,answer){
   var userAnswer = readlineSync.question(question);
+  if (userAnswer===answer){
+    console.log("You are right")
+    score = score+1;
+    console.log("Your score is ",score)
+    console.log("----------------")
+  }
 }
